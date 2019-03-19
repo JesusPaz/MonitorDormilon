e126f069c5559a9bb3c5740d62e3a7a2daf924e4
JesusPaz <jeherpabo@gmail.com>
2019-03-19 15:21:38 -0500

    Agrega estructura y atributos de la Clase Estudiante

--- src/logic/Estudiante.java
+++ src/logic/Estudiante.java
@@ -1,5 +1,36 @@
 package logic;
 
-public class Estudiante {
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
+public class Estudiante extends Thread {
+	
+	private Semaphore Monitor;
+	private Semaphore SalaEspera;
+	private String Nombre;             // El nombre del carro
+	private Random GenAleat;           // Un generador de números aleatorios
+	
+	
+	
+	
+	
+	public Estudiante(Semaphore monitor, Semaphore salaEspera, String nombre, long semilla) {
+		super();
+		Monitor = monitor;
+		SalaEspera = salaEspera;
+		Nombre = nombre;
+		GenAleat = new Random(semilla);
+	}
+
+
+
+
+
+	public void run() {
+			while (true) {
+				
+			}
+	}
+	
 
 }
--- src/logic/Monitor.java
+++ src/logic/Monitor.java
@@ -1,5 +1,19 @@
 package logic;
 
-public class Monitor {
+import java.util.Random;
+
+public class Monitor extends Thread {
+
+	
+	private String Nombre;             // El nombre del carro
+	private Random GenAleat;           // Un generador de números aleatorios
+	
+	
+	
+	
+	public void run() {
+			
+	}
+	
 
 }
